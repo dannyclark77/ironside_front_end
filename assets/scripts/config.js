1700fd7f879eaaeb001f189124aecc0514983395
dannyclark77 <dannyclark77@gmail.com>
2017-09-22 14:37:54 -0400

    Update API origins and npm install

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,8 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://ga-wdi-boston.herokuapp.com'
+    production: 'https://afternoon-earth-95866.herokuapp.com/',
+    development: 'https://localhost4741/'
   }
 }
 
