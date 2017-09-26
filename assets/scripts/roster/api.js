9a6004eae6bb108617c73088f3c37869db8697d4
dannyclark77 <dannyclark77@gmail.com>
2017-09-26 12:48:22 -0400

    Add post request functionality for top 7 team
    -Create api ajax function to send post request
    -Create ui function to signal success and append top 7 list
    -Add functions to get success function to create operational buttons to
    add to top 7 list

--- assets/scripts/roster/api.js
+++ assets/scripts/roster/api.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const config = require('../config')
+const store = require('../store')
 
 const get2008Roster = function () {
   return $.ajax({
@@ -9,6 +10,23 @@ const get2008Roster = function () {
   })
 }
 
+const postTeamRoster = function (players) {
+  return $.ajax({
+    url: config.apiOrigin + '/teams/',
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: {
+      'team': {
+        'user_id': store.user.id,
+        'player_id': players
+      }
+    }
+  })
+}
+
 module.exports = {
-  get2008Roster
+  get2008Roster,
+  postTeamRoster
 }
--- assets/scripts/roster/ui.js
+++ assets/scripts/roster/ui.js
@@ -1,15 +1,34 @@
 'use strict'
 
 const showRosterTemplate = require('../templates/roster-listing.handlebars')
+const store = require('../store')
+const api = require('./api')
 
 const get2008Success = function (data) {
-  console.log(data)
-  const showRosterHtml = showRosterTemplate({ players: data.players })
+  store.players = data.players
+  console.log(store.players)
+  const showRosterHtml = showRosterTemplate({ players: store.players })
   $('.roster').text('')
   $('.roster').append(showRosterHtml)
+  $('.roster').on('click', 'ul', function (event) {
+    event.preventDefault()
+    api.postTeamRoster($(this).data('id'))
+      .then(postTeamSuccess)
+      .catch(postTeamFailure)
+  })
 }
 
 const get2008Failure = function (error) {
+  console.error(error)
+}
+
+const postTeamSuccess = function (data) {
+  console.log(data)
+  $('.topseven').append('<br>')
+  $('.topseven').append(data.team.player.name)
+}
+
+const postTeamFailure = function (error) {
   console.error(error)
 }
 
