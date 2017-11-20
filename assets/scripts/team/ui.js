5ae3806ea64e66e3974d8f3d1c3a2a1a1c695c87
dannyclark77 <dannyclark77@gmail.com>
2017-11-20 18:24:55 -0500

    Add team name to column header

--- assets/scripts/team/ui.js
+++ assets/scripts/team/ui.js
@@ -1,12 +1,14 @@
 'use strict'
 
 const rosterEvents = require('../roster/events')
+const store = require('../store')
 
 const getTeamNameSuccess = function (data) {
   $('#modal-team-name').modal('hide')
   $('#team-name').trigger('reset')
   rosterEvents.signInTopSeven()
   rosterEvents.onDeletePlayer()
+  $('#topseven').text('Team: ' + store.teamName)
 }
 
 const getTeamNameFailure = function (error) {
